6a36bdcd5bce8d51aa7dd67bfe4940cb7bae2891
Varun Shijo <varun.shijo@gmail.com>
2017-02-10 11:16:11 +0530

    Reworked interfacing. Todo: change bubble logic

--- interfacing.py
+++ interfacing.py
@@ -1,7 +1,17 @@
-from gpiozero import Motor, DistanceSensor
+from gpiozero import Motor, DistanceSensor, AngularServo
+from MPU6050 import MPU6050
 
 # ultrasonic
-echo = 15
-trigger = 14
+sonar_echo = 15
+sonar_trigger = 14
+servo_input = 21
+motor_rear_1 = 26
+motor_rear_2 = 19
+#motor_front_1 =
+#motor_front_2 =
 
-sonar = DistanceSensor(echo,trigger)
+imu = MPU6050(0x68)
+sonar = DistanceSensor(sonar_echo,sonar_trigger)
+sweep = AngularServo(servo_input,min_angle=-40,max_angle=45)
+motor_rear = Motor(motor_rear_1,motor_rear_2)
+#motor_front = Motor(motor_front_1,motor_front_2)
---
+++ movement.py
@@ -0,0 +1,35 @@
+from interfacing import motor_rear#,motor_front
+
+linear = 0
+turn = 0
+
+direction = (linear,turn)
+
+def stop():
+	motor_rear.forward(0)
+
+def bot_forward(turn_flag):
+	if turn_flag==0:
+		motor_front.forward(0)
+	elif turn_flag==1:
+		motor_front.forward(1)
+	else:
+		motor_front.backward(1)
+	motor_rear.forward(0.3)
+
+def bot_backward(turn_flag):
+	if turn_flag==0:
+		motor_front.forward(0)
+	elif turn_flag==1:
+		motor_front.forward(1)
+	else:
+		motor_front.backward(1)
+	motor_rear.backward(0.3)
+
+def bot_map(direction):
+	if direction[linear]==0:
+		bot_stop()
+	elif direction[linear]==1:
+		bot_forward(direction[turn])
+	else:
+		bot_reverse(direction[turn])
